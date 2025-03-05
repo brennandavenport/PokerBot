1ccf004d09b3ff1c6d294e06dfc58275a0bf51a0
Brennan Davenport <brennandavenport9@gmail.com>
2025-03-05 14:34:00 -0500

    added dunder methods, str, len

--- src/table.py
+++ src/table.py
@@ -12,8 +12,43 @@ class PlayerNode:
 # Circular linked list that stores the player info
 class Table:
     def __init__(self):
+        """Initializes a Table object, setting the head to None and active player count to 0."""
         self.head = None
         self.active_player_count = 0
+
+
+    def __str__(self):
+        """
+        Returns a string representation of the table's current players and their statuses.
+        
+        If the table has no players, returns a message indicating that. Otherwise, 
+        iterates through all players and returns their name, and whether they are active
+        in the game or round.
+        
+        Returns:
+            str: A string with details about each player in the game.
+        """
+        # If there are no players at the table, return a message
+        if not self.head:
+            return "Table has no players"
+        
+        player_string = ""
+        temp = self.head  # Start from the first player
+        while True:
+            # Concatenate player details to the string
+            player_string += f"Player Name: {temp.data.name}, game active: {temp.data.game_active}, round active: {temp.data.round_active}\n"
+            temp = temp.next  # Move to the next player
+            if temp == self.head:  # Break the loop when we return to the head
+                break
+        
+        return player_string
+
+
+    def __len__(self):
+        """Returns the number of active players at the table."""
+        return self.active_player_count
+
+
 
     def add_players(self, players: List[Player]) -> None:
         for player in players:
@@ -31,6 +66,7 @@ class Table:
                 temp.next = player_node
                 player_node.next = self.head
 
+
     def deal_cards(self, deck) -> None:
         if not self.head:
             return
@@ -40,25 +76,13 @@ class Table:
         while True:
             if temp.data.game_active:
                 temp.data.set_private_cards([deck.deck.pop(), deck.deck.pop()])
-                
+
             temp = temp.next
         
             if temp == self.head:
                 break
         
         
-    def display(self) -> None:
-        if not self.head:
-            print("List is empty")
-            return
-        temp = self.head
-        while True:
-            print(temp.active, end=" -> ")
-            temp = temp.next
-            if temp == self.head:
-                break
-        print("(Back to Head)")
-    
 
     def update_active_players(self, player: Player) -> None:
         #Linked list is empty
@@ -80,13 +104,9 @@ class Table:
                 break
 
 
-
+    # Need to be fixed
     def update_big_blind(self, big_blind: PlayerNode) -> PlayerNode:
-        if self.active_player_count == 1:
-            raise EndOfGameError()
-
-        if not self.head:
-            return None
+        self.validate_player_count(player=big_blind)
         
         temp = big_blind
 
@@ -96,22 +116,29 @@ class Table:
             
             temp = temp.next
 
-    
-    def update_small_blind(self, big_blind: PlayerNode) -> PlayerNode:
-        if self.active_player_count == 1:
-            raise EndOfGameError()
-
-        if not self.head:
-            return None
+    # NEed to be fixed
+    def update_small_blind(self, small_blind: PlayerNode) -> PlayerNode:
+        self.validate_player_count(player=small_blind)
         
-        temp = big_blind
+        temp = small_blind
 
         while True:
-            if temp.active and temp != big_blind:
+            if temp.active and temp != small_blind:
                 return temp
             
             temp = temp.next
 
+    def get_next_player(self, player: PlayerNode) -> PlayerNode:
+        # Throw exception if head is empty and player count is == 1
+        pass
+
+    # IF there is an error then thrwo it
+    def validate_player_count(self, player: PlayerNode) -> None:
+        if not self.head:
+            raise Exception("There are no players")
+        
+        if self.active_player_count <= 1:
+            raise Exception("There are not enought players")
 
 if __name__ == "__main__":
     table = Table()
@@ -126,7 +153,7 @@ if __name__ == "__main__":
     player4 = Player('James', starting_amount, None)
     player5 = Player('Richard', starting_amount, None)
 
-    table.display()
+    print(table)
 
     players = [player1, player2, player3, player4, player5]
 
@@ -140,7 +167,7 @@ if __name__ == "__main__":
     print(big_blind.data.name)
     print(small_blind.data.name)
 
-    table.display()
+    print(table)
 
     big_blind = table.update_big_blind(big_blind)
     small_blind = table.update_small_blind(big_blind)
@@ -148,20 +175,5 @@ if __name__ == "__main__":
     print(big_blind.data.name)
     print(small_blind.data.name)
 
-    table.display()
+    print(table)
 
-    table.update_active_players(player3)
-    table.update_active_players(player4)
-    table.update_active_players(player5)
-    table.update_active_players(player1)
-    try:
-        big_blind = table.update_big_blind(big_blind)
-        small_blind = table.update_small_blind(big_blind)
-    except EndOfGameError:
-        print("Game over")
-        exit()
-
-    print(big_blind.data.name)
-    print(small_blind.data.name)
-
-    table.display()
